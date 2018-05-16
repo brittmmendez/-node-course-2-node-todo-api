d2b3038959ee97d80688366e081a5491a7018d46
Brittany M. Morris <brittmmendez@gmail.com>
2018-05-16 00:35:02 -0400

    added get /todos test

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,13 +4,25 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
-beforeEach((done) => {
-  Todo.remove({}).then(() => done());
-})
+const todos = [{
+  text: 'First test todo'
+}, {
+  text: 'Second test todo'
+}];
 
-describe('Posts /todos', () => {
+beforeEach((done) => {
+ Todo.remove({}).then(() => {
+   return Todo.insertMany(todos, (error, docs) => {
+     if(error){
+       return done(error);
+     }
+   });
+ }).then(() => done());
+});
+
+describe('POST /todos', () => {
   it('should create a new todo', (done) => {
-    let text = 'Test todo text'
+    var text = 'Test todo text';
 
     request(app)
       .post('/todos')
@@ -24,20 +36,18 @@ describe('Posts /todos', () => {
           return done(err);
         }
 
-        Todo.find().then((todos) => {
+        Todo.find({text}).then((todos) => {
           expect(todos.length).toBe(1);
           expect(todos[0].text).toBe(text);
           done();
-        }).catch((e) => done(e))
+        }).catch((e) => done(e));
       });
   });
 
-  it('should not create a todo with invaid body data', (done) => {
-    let text = null
-
+  it('should not create todo with invalid body data', (done) => {
     request(app)
       .post('/todos')
-      .send({text})
+      .send({})
       .expect(400)
       .end((err, res) => {
         if (err) {
@@ -45,10 +55,21 @@ describe('Posts /todos', () => {
         }
 
         Todo.find().then((todos) => {
-          expect(todos.length).toBe(0);
+          expect(todos.length).toBe(2);
           done();
-        }).catch((e) => done(e))
+        }).catch((e) => done(e));
       });
   });
+});
 
+describe('GET /todos', () => {
+  it('should get all todos', (done) => {
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todos.length).toBe(2);
+      })
+      .end(done);
+  });
 });
