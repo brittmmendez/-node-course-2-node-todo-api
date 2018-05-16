e7ea4f9b78df6dfdb34f548945408e28ae209e75
Brittany M. Morris <brittmmendez@gmail.com>
2018-05-16 10:11:48 -0400

    added comments for reference

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 //library imports
-const _ = require('lodash');
-const express = require('express');
-const bodyParser = require('body-parser');
-const {ObjectID} = require('mongodb');
+const _ = require('lodash'); //Lodash makes JavaScript easier by taking the hassle out of working with arrays, numbers, objects, strings, etc.
+const express = require('express'); //Express is a minimal and flexible Node.js web application framework that provides a robust set of features for web and mobile applications.
+const bodyParser = require('body-parser'); //Parse incoming request bodies in a middleware before your handlers
+const {ObjectID} = require('mongodb'); //Create a new ObjectID instance
 
 //local imports
-const {mongoose} = require('./db/mongoose');
+const {mongoose} = require('./db/mongoose'); //Mongoose is a MongoDB object modeling tool designed to work in an asynchronous environment.
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
 
@@ -15,7 +15,7 @@ const app = express();
 //sets up local port or heroku port
 const port = process.env.PORT || 3000;
 
-//takes the body data sent from client json and convert it to an object attaching it on to the request object
+//middleware - takes the body data sent from client json and convert it to an object attaching it on to the request object
 app.use(bodyParser.json());
 
 //CREATE - make new todo by sending JSON obj with text prop to server -> server will take it create document and send back doc
