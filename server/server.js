2495af16285e97ca84788e6d1c257270db0cb039
Brittany M. Morris <brittmmendez@gmail.com>
2018-05-16 10:16:35 -0400

    added comments for reference

--- server/server.js
+++ server/server.js
@@ -1,45 +1,36 @@
 //library imports
-const _ = require('lodash'); //Lodash makes JavaScript easier by taking the hassle out of working with arrays, numbers, objects, strings, etc.
-const express = require('express'); //Express is a minimal and flexible Node.js web application framework that provides a robust set of features for web and mobile applications.
-const bodyParser = require('body-parser'); //Parse incoming request bodies in a middleware before your handlers
-const {ObjectID} = require('mongodb'); //Create a new ObjectID instance
+const _ = require('lodash');      //Lodash makes JavaScript easier by taking the hassle out of working with arrays, numbers, objects, strings, etc.
+const express = require('express');      //Express is a minimal and flexible Node.js web application framework that provides a robust set of features for web and mobile applications.
+const bodyParser = require('body-parser');      //Parse incoming request bodies in a middleware before your handlers
+const {ObjectID} = require('mongodb');      //Create a new ObjectID instance
 
 //local imports
-const {mongoose} = require('./db/mongoose'); //Mongoose is a MongoDB object modeling tool designed to work in an asynchronous environment.
+const {mongoose} = require('./db/mongoose');      //Mongoose is a MongoDB object modeling tool designed to work in an asynchronous environment.
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
 
 
-//stores the express application
-const app = express();
-//sets up local port or heroku port
-const port = process.env.PORT || 3000;
+const app = express();      //stores the express application
+const port = process.env.PORT || 3000      //sets up local port or heroku port
 
-//middleware - takes the body data sent from client json and convert it to an object attaching it on to the request object
-app.use(bodyParser.json());
+app.use(bodyParser.json());     //middleware - takes the body data sent from client json and convert it to an object attaching it on to the request object
 
 //CREATE - make new todo by sending JSON obj with text prop to server -> server will take it create document and send back doc
 app.post('/todos', (req, res) => {
-  //create an instance of mongoose model
-  let newTodo = new Todo({
+  let newTodo = new Todo({     //create an instance of mongoose model
     text: req.body.text
   });
 
-  //save model to db
-  newTodo.save().then((doc) =>{
+  newTodo.save().then((doc) =>{     //save model to db
     res.send(doc)
   }, (e) => {
     res.status(400).send(e);
   });
 });
 
-
-
-
 //READ
   app.get('/todos', (req, res) => {
-    //create an instance of mongoose model
-    Todo.find().then((todos) =>{
+    Todo.find().then((todos) =>{    //create an instance of mongoose model
       res.send({todos});
     }, (e) => {
       res.status(400).send(e);
@@ -50,16 +41,13 @@ app.post('/todos', (req, res) => {
 app.get('/todos/:id', (req, res) => {
   let id = req.params.id;
 
-  //validate id
-  if (!ObjectID.isValid(id)) {
+  if (!ObjectID.isValid(id)) {      //validate id
     return res.status(404).send();
   };
 
-  //create an instance of mongoose model
-  Todo.findById(id).then((todo) => {
+  Todo.findById(id).then((todo) => {    //create an instance of mongoose model
 
-    //handles the error if ID isn't found
-    if (!todo) {
+    if (!todo) {      //handles the error if ID isn't found
       return res.status(404).send();
     }
 
@@ -69,22 +57,17 @@ app.get('/todos/:id', (req, res) => {
   });
 });
 
-
-
-
 //DELETE by ID
 app.delete('/todos/:id', (req, res) => {
   let id = req.params.id;
-  //validate id
-  if (!ObjectID.isValid(id)) {
+
+  if (!ObjectID.isValid(id)) {      //validate id
     return res.status(404).send();
   };
 
-  //create an instance of mongoose model
-  Todo.findByIdAndRemove(id).then((todo) => {
+  Todo.findByIdAndRemove(id).then((todo) => {   //create an instance of mongoose model
 
-    //handles the error if ID isn't found
-    if (!todo) {
+    if (!todo) {      //handles the error if ID isn't found
       return res.status(404).send();
     }
 
@@ -100,27 +83,21 @@ app.delete('/todos/:id', (req, res) => {
   app.patch('/todos/:id', (req, res) => {
     let id = req.params.id;
 
-    //needed lodash for this specifically so users can't change things I don't want them to.
-    //This checks the body of the request and picks the two items in my array and will only update this
-    let body = _.pick(req.body, ['text', 'completed']);
+    let body = _.pick(req.body, ['text', 'completed']);     //needed lodash for this specifically so users can't change things I don't want them to. This checks the body of the request and picks the two items in my array and will only update this
 
-    //validate id
-    if (!ObjectID.isValid(id)) {
+    if (!ObjectID.isValid(id)) {      //validate id
       return res.status(404).send();
     };
 
-    //updated the completed at based on body prop
-    if (_.isBoolean(body.completed) && body.completed) {
+    if (_.isBoolean(body.completed) && body.completed) {    //updated the completed at based on body prop
       body.completedAt = new Date().getTime();
     }else{
       body.completed = false;
       body.completedAt = null;
     }
 
-    //make our call to find by id and update
-    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
-      //handles the error if ID isn't found
-      if (!todo) {
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {  //make our call to find by id and update
+      if (!todo) {    //handles the error if ID isn't found
         return res.status(404).send();
       }
 
